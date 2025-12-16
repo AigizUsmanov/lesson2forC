b3276e5841ecaab8f7622f1b0340700adaf368b1
AigizUsmanov <aigizusmanov@mail.ru>
2025-12-16 15:17:11 +0300

    lesoon4 - Условные операторы if/ else

--- lesson2forC/main.c
+++ lesson2forC/main.c
@@ -1,9 +1,27 @@
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
+    int x = 20, y = 5;
     
-    printf("Hello, World!\n");
+    if ((x == 10 && y < 7) || x == 20)
+        printf("Yes\n");
+      else  if (x > 100) {
+            if(x == 105)
+                printf("X is equel 105\n");
+          printf("X is greater 100\n");
+          
+        } else if(x < 5)
+            printf("X is less than 5\n");
+            else
+    printf("No\n");
     
+    bool isHasCar = true;
+    if(isHasCar == true) // if(isHasCar) это аналогичная запись
+    // if(isHasCar == false)  так же аналогично if(!isHisCar)
+        printf("Yes\n");
+    else
+        printf("No\n");
     return 0;
 }
