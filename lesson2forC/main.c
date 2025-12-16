5779439a54208160a99d34c1180971fd55d35deb
AigizUsmanov <aigizusmanov@mail.ru>
2025-12-16 13:40:20 +0300

    Initial Commit

---
+++ lesson2forC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lesson2forC
+//
+//  Created by Админ on 16/12/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
