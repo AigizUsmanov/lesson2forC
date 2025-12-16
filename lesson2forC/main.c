434519927b980dcdd7cdc41c9d6d54ce127b93c4
AigizUsmanov <aigizusmanov@mail.ru>
2025-12-16 14:21:42 +0300

    Delete comment

--- lesson2forC/main.c
+++ lesson2forC/main.c
@@ -1,14 +1,9 @@
-//
-//  main.c
-//  lesson2forC
-//
-//  Created by Админ on 16/12/2025.
-//
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int main() {
+    
     printf("Hello, World!\n");
+    
     return 0;
 }
